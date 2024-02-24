19aea4afc1afb8ed99f1984b6203615d32d25b4f
Qianming <2468116431@qq.com>
2024-02-24 13:55:09 +0800

    forge 打包

--- src/main/main.ts
+++ src/main/main.ts
@@ -7,41 +7,43 @@ import { getPathByDialog } from './dialog';
 import { useEvent } from './hook/useEvent';
 import { detectDevices } from './devices/detectDevices';
 
-function createWindow() {
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+
   useExpress()
 
   useEvent()
 
   detectDevices().then((res) => console.log(res))
 
+
+  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 780,
+    width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      contextIsolation: true
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
   });
 
-  // 开启控制台调试工具
-
-  setTimeout(() => {
-    if (process.env.NODE_ENV === "development") {
-     mainWindow.webContents.openDevTools({ mode: 'undocked' })
-    }
-  }, 600);
-
-
-  if (process.env.NODE_ENV === 'development') {
-    const rendererPort = process.argv[2];
-    mainWindow.loadURL(`http://localhost:${rendererPort}`);
-  }
-  else {
-    mainWindow.loadFile(join(app.getAppPath(), 'renderer', 'index.html'));
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
-}
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+};
+
 
 app.whenReady().then(() => {
   createWindow();
